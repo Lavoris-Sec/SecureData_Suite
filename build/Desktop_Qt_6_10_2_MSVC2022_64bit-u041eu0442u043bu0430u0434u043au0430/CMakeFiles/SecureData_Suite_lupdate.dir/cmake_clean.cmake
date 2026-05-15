d3cbfe40f67ce6fcd28d333f94762c82c406d3be
Lavoris-Sec <aliaksei.kaliankou.devsec@gmail.com>
2026-05-15 11:23:26 +0300

    Add proper gitignore and stop tracking build artifacts

--- build/Desktop_Qt_6_10_2_MSVC2022_64bit-u041eu0442u043bu0430u0434u043au0430/CMakeFiles/SecureData_Suite_lupdate.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/SecureData_Suite_lupdate"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SecureData_Suite_lupdate.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
